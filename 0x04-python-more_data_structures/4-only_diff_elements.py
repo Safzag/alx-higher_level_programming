7c6af80cd15bff8ed99051a152093d4c4a102f06
Safzag <safaezghari100@gmail.com>
2023-11-08 15:47:28 -0800

    4-only_diff_elements.py

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrating how to return a set of all elements present in only one set
+#
+# (C) 2022 safae zghari, Sale, Morocco
+# email safazghari100@gmail.com
+# -----------------------------------------------------------
+
+
+def only_diff_elements(set_1, set_2):
+    return set_1 ^ set_2
