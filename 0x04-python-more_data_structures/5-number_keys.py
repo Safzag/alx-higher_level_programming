5d2d25dbfd3c7aab34c1fd15775e1254f446a2b0
Safzag <safaezghari100@gmail.com>
2023-11-08 15:55:33 -0800

    5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrating how to return the number of keys in a dictionary
+#
+# (C) 2023 safae zghari , Sale , Morocco
+# email safaezghari100@gmail.com
+# -----------------------------------------------------------
+
+
+def number_keys(a_dictionary):
+    return len(a_dictionary.keys())
