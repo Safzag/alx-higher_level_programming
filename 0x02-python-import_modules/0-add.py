64455476bca35c2b2f250a9727e5fd666933f36a
Safzag <safaezghari100@gmail.com>
2023-11-08 16:36:28 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+# -----------------------------------------------------------
+# Python program that:
+# demonstrating how to return a list with all values multiplied by a number
+# without using any loops
+#
+# (C) 2023 zghari safae, Sale, Morocco
+# email safaezghari100@gmail.com
+# -----------------------------------------------------------
+
+
+# Import the add function
+from task import add
+
+a = 1
+b = 2
+
+# This code should not run when this file is imported
 if __name__ == "__main__":
-    from add_0 import add
-
-    a = 1
-    b = 2
-
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}". format(a, b, add(a, b)))
