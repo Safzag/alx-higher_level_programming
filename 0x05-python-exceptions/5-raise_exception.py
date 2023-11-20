65f1da3c8b505344a6565e72641289589b5ec959
Safzag <safaezghari100@gmail.com>
2023-11-20 15:29:47 -0800

    5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError
+
+
+
+
+6-main.py
+
+#!/usr/bin/python3
+raise_exception_msg = __import__('6-raise_exception_msg').raise_exception_msg
+
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
